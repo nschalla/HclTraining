5e12b9ba2ebd658c7b232e4f1cbd6233730041a6
Narayanaswamy challa <36071265+nschalla@users.noreply.github.com>
2020-10-09 09:11:30 +0530

    Add files via upload

---
+++ VALIDATION EVENT/script.js
@@ -0,0 +1,31 @@
+var eventDetails=[];
+function addEvent(){
+
+    var event=document.getElementById("eventName").value;
+    if(eventDetails.includes(event)){
+        document.getElementById("successMessage").innerHTML="EVENT ALREADY EXISTS";
+    }
+    else{
+        eventDetails.push(event);
+        document.getElementById("successMessage").innerHTML="EVENT IS SUCCESSFULLY INSERTED";
+
+    }
+function displayEvents(){
+    console.log("table");
+    var result=document.getElementById("results");
+    var h3=document.createElement("h3");
+    h3.innerHTML="EVENT DETAILS";
+    h3.appendChild(result);
+    var table=document.createElement("table");
+    table.style.border="1px solid";
+    table.appendChild(result);
+    var tableheader=document.createElement("th");
+    th.appendChild(table);
+    for(var i=0;i<eventDetails.length;i++){
+        var td =document.createElement("td");
+        td.appendChild(table);
+        td.innerHTML=eventDetails[i];
+    }
+}
+
+}
